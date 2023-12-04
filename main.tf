238b6e98079aa32b72dd0633a92e1a6bf8aaafb8
fiction <footedroom575@gmail.com>
2023-12-04 10:58:41 +0000

    feat: now able to assign vpc name dynamically

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@
 module "vpc" {
   source = "./modules/vpc"
 
+  vpc_name = "Energy Microservices Project"
   vpc_cidr        = "10.0.0.0/20"
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   private_subnets = ["10.0.8.0/24", "10.0.9.0/24", "10.0.10.0/24"]
